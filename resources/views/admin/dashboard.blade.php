9d437a5b8f502f5b554aca1fef5e85a7815c3eb4
porphyraa <hernandoneo@yahoo.com>
2022-05-26 06:16:14 +0700

    ver 1.1

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,12 +8,22 @@
 		
 	<h2>Halo, Admin!</h2>
 	<button type="button"><a href="{{ route('admin.add') }}">Tambah Calon</a></button>
+	<form action="{{ route('admin.logout') }}" method="POST" >
+		@csrf
+		<input type="submit" value="Keluar">
+	</form>
 	<br><br><br>
+	<!-- Tabel Calon Kades -->
 	<table>
 		<tr>
 		@forelse($calons as $calon)
 			<td>
 				<table style="border: 1px solid">
+					<tr>
+						<td>
+							<img src='/image/calon/{{$calon->photoName}}'/>
+						</td>
+					</tr>
 					<tr>
 						<td>{{ $calon->name }}</td>
 					</tr>
@@ -22,8 +32,8 @@
 					</tr>
 					<tr>
 						<td>
-							<a type="button">Edit</button>
-							<a href='delete/{{ $calon->id }}'>Hapus</button>
+							<a href='edit/{{ $calon->id }}'>Edit</a>
+							<a href='delete/{{ $calon->id }}'>Hapus</a>
 						</td>
 					</tr>
 				</table>
@@ -33,6 +43,19 @@
 			@endforelse
 		</tr>
 	</table>
+	<br><br><br>
+	<!-- Statistik Calon Kades -->
+	<p>Jumlah pemilih: {{$electionData['totalUser']}}</p>
+	<p>Jumlah calon: {{$electionData['totalCalon']}}</p>
+	<p>Statistik: <br>
+		<ul>
+			@forelse($calons as $calon)
+				<li>{{ $calon->name }}: {{ $calon->totalVote }}</li>
+			@empty
+			@endforelse
+				<li>Pemilih yang belum memilih: {{$electionData['totalUser'] - $electionData['totalUserHasVoted'] }}</li>
+		</ul>
+	</p>
 </body>
 </html>
 
